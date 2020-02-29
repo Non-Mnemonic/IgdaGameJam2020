974b43cf66259747381d8984ca48025cf99ef050
Catloaf <thecatloaf@gmail.com>
2020-02-29 12:13:03 -0500

    I hope this works right this time...

---
+++ Assets/Scripts/InTheLightBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InTheLightBehavior : MonoBehaviour
+{
+    public List<Light> lightList;   //All lights that we want to consider, in the inspector you can just drag and drop them over the name to add them to the list
+    public bool illuminated;    //Is this object in light or shadow
+
+    private GameObject lightableObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightableObject = this.GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Light lightInstance in lightList)
+        {
+            switch (lightInstance.type) {   //I doubt we'll be using anything other than directional/Spotlights, but It's easy enough for most of these... not doing Disk
+                case LightType.Directional:
+                    Debug.LogError("Please don't make me do this one!");
+                    break;
+                case LightType.Disc:
+                    Debug.LogError("Nothing defined for LightType Disk");
+                    break;
+                case LightType.Point:
+
+                    break;
+                case LightType.Rectangle:
+                    Debug.LogError("Nothing defined for LightType REctangle");
+                    break;
+                case LightType.Spot:
+
+                    break;
+
+            }
+            
+            
+
+        }   
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpSpeed;
+    public float jumpHieght;
+    public float moveSpeed;
+    public float fallSpeed;
+    //private GameObject player;
+    private Rigidbody playerRidgidBody;
+    private Transform cameraTransform;
+    private Vector3 movementDirection;
+    private bool canJump = true;
+    [SerializeField]
+    private float jumpMovement;
+
+    void Start()
+    {
+        //player = GetComponent<GameObject>();
+        playerRidgidBody = GetComponent<Rigidbody>();
+        cameraTransform = Camera.main.transform;
+        movementDirection = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("up"))
+        {
+            movementDirection.z = moveSpeed;
+        }
+        if (Input.GetKeyUp("w")||Input.GetKeyUp("up"))
+        {
+            movementDirection.z = 0;
+        }
+        if (Input.GetKeyDown("a") || Input.GetKeyDown("left"))
+        {
+            movementDirection.x = -moveSpeed;
+        }
+        if (Input.GetKeyUp("a")||Input.GetKeyUp("left"))
+        {
+            movementDirection.x = 0;
+        }
+        if (Input.GetKeyDown("s") || Input.GetKeyDown("down"))
+        {
+            movementDirection.z = -moveSpeed;
+        }
+        if (Input.GetKeyUp("s")||Input.GetKeyUp("down"))
+        {
+            movementDirection.z = 0;
+        }
+        if (Input.GetKeyDown("d") || Input.GetKeyDown("right"))
+        {
+            movementDirection.x = moveSpeed;
+        }
+        if (Input.GetKeyUp("d")||Input.GetKeyUp("right"))
+        {
+            movementDirection.x = 0;
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            if (playerRidgidBody.transform.position.y < jumpHieght && canJump)
+            {
+                jumpMovement = jumpSpeed;
+            }            
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            jumpMovement = -fallSpeed;
+            canJump = false;
+        }
+
+        if (playerRidgidBody.transform.position.y >= jumpHieght)
+        {
+            jumpMovement = -fallSpeed;
+            canJump = false;
+        }
+
+
+        Vector3 move = cameraTransform.TransformVector(movementDirection);
+        move.y = jumpMovement;
+        playerRidgidBody.velocity = move;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        jumpMovement = 0;
+        canJump = true;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        canJump = true;
+    }
+}
