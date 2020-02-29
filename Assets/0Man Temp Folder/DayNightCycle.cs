297578eb5b12e71b7b45e92dc07b7d067270ae73
0-Man <othman1997@gmail.com>
2020-02-29 11:00:28 -0500

    finished sun and moon cycle with a speed changer (default speed 1 is 3 mins till sun down)

---
+++ Assets/0Man Temp Folder/DayNightCycle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    // create a countdown for 3 mins
+
+    public float speed = 1f;
+    private float time;
+    private float timer = 0.0f;
+
+
+    void Start()
+    {
+        //create default timer for 3 mins
+        time = 180f / speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        transform.RotateAround(Vector3.zero, Vector3.forward, speed*0.004f);
+        transform.LookAt(Vector3.zero);
+
+        if (timer > time)
+        {
+            this.gameObject.GetComponent<GameController>().StateChange();
+            timer = timer - time;
+        }
+        
+    }
+
+    //Note: night time is positive rotation, day is negative rotation.
+
+ 
+}
